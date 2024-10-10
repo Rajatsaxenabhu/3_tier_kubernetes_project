3298934c351cf22382d236130236fd55dc871243
Rajatsaxenabhu <saxenarajat499@gmail.com>
2024-10-10 23:58:15 +0530

    phase_2

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 3_tier_kubernetes_project
+
+#  Terraform
+------------------------
+<!-- # source .env   // for aws config -->
+<!-- # sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo -->
+<!-- # sudo dnf install terraform -->
+
+# AWS
+-----------------
+# ssh -i pem_filename ubuntu@<ipaddress>
+# git clone https://github.com/Rajatsaxenabhu/3_tier_kubernetes_project.git
+# create the backend and frontend images 
+# install the kubectl and eksctl    
---
+++ terraform/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+// To Generate Private Key
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+}
+
+// Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+// Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+# Create Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "my_server" {
+  ami           = "ami-0866a3c8686eaeeba"  # Make sure this AMI is valid in your region
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key_pair.key_name  # Reference the key pair
+  security_groups = [aws_security_group.allow_ssh.name]  # Attach security group
+
+  tags = {
+    name = "3_tier"
+  }
+}
+
+
+
+# Output EC2 Instance Public IP
+output "instance_ip" {
+  value = aws_instance.my_server.public_ip
+}
